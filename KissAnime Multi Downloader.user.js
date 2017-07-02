eed2981e8203e525a9b3c1658569163aaf2d07ba
AnimeBro <shtyllat@gmail.com>
2017-07-02 11:49:53 +0200

    Update KissAnime Multi Downloader.user.js

--- KissAnime Multi Downloader.user.js
+++ KissAnime Multi Downloader.user.js
@@ -12,8 +12,8 @@
 // @grant        GM_xmlhttpRequest
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 
-// @require      https://rawgit.com/Eltion/Kissanime-Downloader/master/css.js
-// @require      https://rawgit.com/Eltion/Kissanime-Downloader/master/vr.js
+// @require      https://cdn.rawgit.com/Eltion/Kissanime-Downloader/8bad11bd/css.js
+// @require      https://cdn.rawgit.com/Eltion/Kissanime-Downloader/8bad11bd/vr.js
 
 // ==/UserScript==
 
