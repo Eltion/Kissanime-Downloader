2cb2e39bdc1d1dc30eaeff203661ae57072370e4
AnimeBro <shtyllat@gmail.com>
2017-08-04 00:17:01 +0200

    Create vr2.js

---
+++ vr2.js
@@ -0,0 +1,96 @@
+var animebro2 = 0; 
+(function(_0xde42x1) {
+     for (var _0xde42x2 = CryptoJS, _0xde42x3 = _0xde42x2['lib'], _0xde42x4 = _0xde42x3['WordArray'], _0xde42x5 = _0xde42x3['Hasher'], _0xde42x3 = _0xde42x2['algo'], _0xde42x6 = [], _0xde42x7 = [], _0xde42x8 = function(_0xde42xb) {
+             return 4294967296 * (_0xde42xb - (_0xde42xb | 0)) | 0
+         }, _0xde42x9 = 2, _0xde42xa = 0; 64 > _0xde42xa;) {
+         var _0xde42xc;
+         a: {
+             _0xde42xc = _0xde42x9;
+             for (var _0xde42xd = _0xde42x1['sqrt'](_0xde42xc), _0xde42xe = 2; _0xde42xe <= _0xde42xd; _0xde42xe++) {
+                 if (!(_0xde42xc % _0xde42xe)) {
+                     _0xde42xc = !1;
+                     break a
+                 }
+             };_0xde42xc = !0
+         }
+         _0xde42xc && (8 > _0xde42xa && (_0xde42x6[_0xde42xa] = _0xde42x8(_0xde42x1['pow'](_0xde42x9, 0.5))), _0xde42x7[_0xde42xa] = _0xde42x8(_0xde42x1['pow'](_0xde42x9, 1 / 3)), _0xde42xa++);
+         _0xde42x9++
+     };
+     var _0xde42xf = [],
+         _0xde42x3 = _0xde42x3['SHA256'] = _0xde42x5['extend']({
+             _doReset: function() {
+                 this['_hash'] = new _0xde42x4['init'](_0xde42x6['slice'](0))
+             },
+             _doProcessBlock: function(_0xde42xb, _0xde42x3) {
+                 for (var _0xde42x10 = this['_hash']['words'], _0xde42x11 = _0xde42x10[0], _0xde42xc = _0xde42x10[1], _0xde42xa = _0xde42x10[2], _0xde42x1 = _0xde42x10[3], _0xde42x12 = _0xde42x10[4], _0xde42x2 = _0xde42x10[5], _0xde42x5 = _0xde42x10[6], _0xde42x9 = _0xde42x10[7], _0xde42x13 = 0; 64 > _0xde42x13; _0xde42x13++) {
+                     if (16 > _0xde42x13) {
+                         _0xde42xf[_0xde42x13] = _0xde42xb[_0xde42x3 + _0xde42x13] | 0
+                     } else {
+                         var _0xde42x14 = _0xde42xf[_0xde42x13 - 15],
+                             _0xde42x15 = _0xde42xf[_0xde42x13 - 2];
+                         _0xde42xf[_0xde42x13] = ((_0xde42x14 << 25 | _0xde42x14 >>> 7) ^ (_0xde42x14 << 14 | _0xde42x14 >>> 18) ^ _0xde42x14 >>> 3) + _0xde42xf[_0xde42x13 - 7] + ((_0xde42x15 << 15 | _0xde42x15 >>> 17) ^ (_0xde42x15 << 13 | _0xde42x15 >>> 19) ^ _0xde42x15 >>> 10) + _0xde42xf[_0xde42x13 - 16]
+                     };
+                     _0xde42x14 = _0xde42x9 + ((_0xde42x12 << 26 | _0xde42x12 >>> 6) ^ (_0xde42x12 << 21 | _0xde42x12 >>> 11) ^ (_0xde42x12 << 7 | _0xde42x12 >>> 25)) + (_0xde42x12 & _0xde42x2 ^ ~_0xde42x12 & _0xde42x5) + _0xde42x7[_0xde42x13] + _0xde42xf[_0xde42x13];
+                     _0xde42x15 = ((_0xde42x11 << 30 | _0xde42x11 >>> 2) ^ (_0xde42x11 << 19 | _0xde42x11 >>> 13) ^ (_0xde42x11 << 10 | _0xde42x11 >>> 22)) + (_0xde42x11 & _0xde42xc ^ _0xde42x11 & _0xde42xa ^ _0xde42xc & _0xde42xa);
+                     _0xde42x9 = _0xde42x5;
+                     _0xde42x5 = _0xde42x2;
+                     _0xde42x2 = _0xde42x12;
+                     _0xde42x12 = _0xde42x1 + _0xde42x14 | 0;
+                     _0xde42x1 = _0xde42xa;
+                     _0xde42xa = _0xde42xc;
+                     _0xde42xc = _0xde42x11;
+                     _0xde42x11 = _0xde42x14 + _0xde42x15 | 0
+                 };
+                 _0xde42x10[0] = _0xde42x10[0] + _0xde42x11 | 0;
+                 _0xde42x10[1] = _0xde42x10[1] + _0xde42xc | 0;
+                 _0xde42x10[2] = _0xde42x10[2] + _0xde42xa | 0;
+                 _0xde42x10[3] = _0xde42x10[3] + _0xde42x1 | 0;
+                 _0xde42x10[4] = _0xde42x10[4] + _0xde42x12 | 0;
+                 _0xde42x10[5] = _0xde42x10[5] + _0xde42x2 | 0;
+                 _0xde42x10[6] = _0xde42x10[6] + _0xde42x5 | 0;
+                 _0xde42x10[7] = _0xde42x10[7] + _0xde42x9 | 0
+             },
+             _doFinalize: function() {
+                 var _0xde42xc = this['_data'],
+                     _0xde42xa = _0xde42xc['words'],
+                     _0xde42x10 = 8 * this['_nDataBytes'],
+                     _0xde42x11 = 8 * _0xde42xc['sigBytes'];
+                 _0xde42xa[_0xde42x11 >>> 5] |= 128 << 24 - _0xde42x11 % 32;
+                 _0xde42xa[(_0xde42x11 + 64 >>> 9 << 4) + 14] = _0xde42x1['floor'](_0xde42x10 / 4294967296);
+                 _0xde42xa[(_0xde42x11 + 64 >>> 9 << 4) + 15] = _0xde42x10;
+                 _0xde42xc['sigBytes'] = 4 * _0xde42xa['length'];
+                 this._process();
+                 return this['_hash']
+             },
+             clone: function() {
+                 var _0xde42xa = _0xde42x5['clone']['call'](this);
+                 _0xde42xa['_hash'] = this['_hash']['clone']();
+                 return _0xde42xa
+             }
+         });
+     _0xde42x2['SHA256'] = _0xde42x5._createHelper(_0xde42x3);
+     _0xde42x2['HmacSHA256'] = _0xde42x5._createHmacHelper(_0xde42x3)
+ })(Math);
+ var bkZ = 'a5e8d2e9c1721ae0e84ad660c472c1f3';
+ var skH = 'nhasasdbasdtene7230asb';
+ var iv, key;
+ iv = CryptoJS['enc']['Hex']['parse'](bkZ);
+ key = CryptoJS.SHA256(skH);
+
+ function ovelWrap(_0xde42x1b) {
+     var _0xde42x1c = null;
+     try {
+         var _0xde42x1d = CryptoJS['lib']['CipherParams']['create']({
+             ciphertext: CryptoJS['enc']['Base64']['parse'](_0xde42x1b)
+         });
+         var _0xde42x1e = CryptoJS['AES']['decrypt'](_0xde42x1d, key, {
+             mode: CryptoJS['mode']['CBC'],
+             iv: iv,
+             padding: CryptoJS['pad']['Pkcs7']
+         });
+         _0xde42x1c = _0xde42x1e.toString(CryptoJS['enc'].Utf8);
+         return _0xde42x1c
+     } catch (err) {
+         animebro2 = 1;
+     }
+ }
